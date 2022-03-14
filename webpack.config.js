837bd641b6808ad48eb6ebbd41e2710904ebaff5
dmkirshon <kirshon.david@gmail.com>
2022-03-14 14:33:07 +0200

    chore: Set up initial dev environment

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    assetModuleFilename: "images/[hash][ext][query]",
+    clean: true,
+  },
+  mode: "development",
+  plugins: [new HtmlWebpackPlugin({ title: "Battleship" })],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
